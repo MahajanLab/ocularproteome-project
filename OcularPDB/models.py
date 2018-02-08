78933c6b17bfd9dad2308e1032afcc050a896198
danielmachlab <daniel@hadlim.com>
2018-02-08 10:54:40 -0600

    Created models class Protein

--- OcularPDB/models.py
+++ OcularPDB/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Protein(models.Model):
+    pID = models.CharField
+    pName = models.CharField
+    pFoveaAvg = models.IntegerField
+    pMaculaAvg = models.IntegerField
+    pPeripheryAvg = models.IntegerField
+
