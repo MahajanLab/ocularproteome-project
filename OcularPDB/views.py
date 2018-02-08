1c9a764641a3c5e385f78c518a2bd755ced61e5d
Daniel Machlab <daniel@hadlim.com>
2018-02-08 10:30:38 -0600

    Submit button now directs user to results page.

--- OcularPDB/views.py
+++ OcularPDB/views.py
@@ -5,8 +5,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+def results(request):
+    return HttpResponse("OH FUCk swild")
+    #return render(request, "ocular_proteome_db/results.html")
+
+
 def index(request):
-    return render(request, "ocular_proteome_db/home.html")
+    return render(request, "ocular_proteome_db/results.html")
+
+
 
 
 # def secret(name):
