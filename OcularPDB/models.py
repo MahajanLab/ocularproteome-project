72adfc0a7ebfb538260969566816be00835cee50
Ted Paulsen <ted.m.paulsen@gmail.com>
2018-02-13 09:47:30 -0600

    updated models

--- OcularPDB/models.py
+++ OcularPDB/models.py
@@ -1,9 +1,25 @@
 from django.db import models
 
 
+# all proteins have an ensembl ID
+# I think this is a good way to do things because then when we
+# search for a protein by its ens_id, we only have to look in one place
+# rather than having to search each model until we find it
 class Protein(models.Model):
-    pID = models.CharField
-    pName = models.CharField
-    pFoveaAvg = models.IntegerField
-    pMaculaAvg = models.IntegerField
-    pPeripheryAvg = models.IntegerField
+    ens_id = models.CharField(primary_key=True)
+
+
+# inherits from base class called Protein
+class RCProtein(Protein):
+    name = models.CharField(unique=True)
+    fovea_avg = models.IntegerField
+    macula_avg = models.IntegerField
+    periphery_avg = models.IntegerField
+
+
+# inherits from base class called Protein
+class VProtein(Protein):
+    post_hyaloid_avg = models.IntegerField
+    ant_hyaloid_avg = models.IntegerField
+    vit_base_avg = models.IntegerField
+    vit_core_avg = models.IntegerField
--- ocular_proteome_db/settings.py
+++ ocular_proteome_db/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'OcularPDB.apps.OcularpdbConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
