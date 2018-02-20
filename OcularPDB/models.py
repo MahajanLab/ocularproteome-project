6c0128791eaf609116f27e739d88304308204761
Ted Paulsen <ted.m.paulsen@gmail.com>
2018-02-20 12:07:09 -0600

    Temporarily simplified models, updated views.py to get protein from db

--- OcularPDB/models.py
+++ OcularPDB/models.py
@@ -5,21 +5,29 @@ from django.db import models
 # I think this is a good way to do things because then when we
 # search for a protein by its ens_id, we only have to look in one place
 # rather than having to search each model until we find it
-class Protein(models.Model):
-    ens_id = models.CharField(primary_key=True, max_length=200)
+# class Protein(models.Model):
+#     ens_id = models.CharField(primary_key=True, max_length=200, default=0)
+#
+#
+# # inherits from base class called Protein
+# class RCProtein(Protein):
+#     name = models.CharField(unique=True, max_length=200)
+#     fovea_avg = models.IntegerField()
+#     macula_avg = models.IntegerField()
+#     periphery_avg = models.IntegerField()
+#
+#
+# # inherits from base class called Protein
+# class VProtein(Protein):
+#     post_hyaloid_avg = models.IntegerField()
+#     ant_hyaloid_avg = models.IntegerField()
+#     vit_base_avg = models.IntegerField()
+#     vit_core_avg = models.IntegerField()
 
 
-# inherits from base class called Protein
-class RCProtein(Protein):
-    name = models.CharField(unique=True, max_length=200)
-    fovea_avg = models.IntegerField
-    macula_avg = models.IntegerField
-    periphery_avg = models.IntegerField
-
-
-# inherits from base class called Protein
-class VProtein(Protein):
-    post_hyaloid_avg = models.IntegerField
-    ant_hyaloid_avg = models.IntegerField
-    vit_base_avg = models.IntegerField
-    vit_core_avg = models.IntegerField
+class RetinaProtein(models.Model):
+    ens_id = models.CharField(primary_key=True, max_length=255, default='x')
+    name = models.CharField(unique=True, max_length=255)
+    fovea_avg = models.IntegerField()
+    macula_avg = models.IntegerField()
+    periphery_avg = models.IntegerField()
--- OcularPDB/views.py
+++ OcularPDB/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from OcularPDB.models import RetinaProtein
 
 
 def results(request):
-    return render(request, "ocular_proteome_db/results.html")
+    identifier = request._post['identifier']
+    proteins = RetinaProtein.objects.get(pk=identifier)
+    vars = {'proteins': proteins}
+
+    return render(request, "ocular_proteome_db/results.html", context=vars)
 
 
 def index(request):
