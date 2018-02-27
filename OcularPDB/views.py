4efaac9ca4554a38bae4e810a4730215e066fb07
Ted Paulsen <ted.m.paulsen@gmail.com>
2018-02-27 10:19:14 -0600

    updated views and results page to display correctly

--- OcularPDB/views.py
+++ OcularPDB/views.py
@@ -10,19 +10,19 @@ def results(request):
 
     print(protein_array[0])
     protein = RetinaProtein.objects.get(pk=protein_array[0])  # return a RetinaProtein object
-    protein_dict = {protein_array[0]: protein}  # add the RetinaProtein object to dict which will hold all the proteins
+    protein_list = [protein]  # add the RetinaProtein object to dict which will hold all the proteins
 
     for i in range(1, len(protein_array)):
         try:
             protein = RetinaProtein.objects.get(pk=protein_array[i])  # return a RetinaProtein object
-            dict_el = {protein_array[i]: protein}  # add RetinaProtein object to a dict object
-            print(dict_el)
-            protein_dict.update(dict_el)  # update dict with dict object containing the last protein
+            protein_list.append(protein)  # update dict with dict object containing the last protein
         except:
             print("error", protein_array[i])
             error_proteins.append(protein_array[i])  # if the protein is not found in the database, add it to the list
 
-    return render(request, "ocular_proteome_db/results.html", context=protein_dict)
+    data = {'protein_list': protein_list}
+
+    return render(request, "ocular_proteome_db/results.html", context=data)
 
 
 def index(request):
