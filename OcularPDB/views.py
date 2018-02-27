81c3cd6d064f39569569051b8fe9296b5f9afc61
Ted Paulsen <ted.m.paulsen@gmail.com>
2018-02-27 10:43:28 -0600

    added error list rendering

--- OcularPDB/views.py
+++ OcularPDB/views.py
@@ -18,9 +18,10 @@ def results(request):
             protein_list.append(protein)  # append new protein to list
         except:
             print("error", protein_array[i])
-            error_proteins.append(protein_array[i])  # if the protein is not found in the database, add it to the list
+            error_proteins.append(protein_array[i] + ' ')  # if the protein is not found in the database, add it to the list
 
-    data = {'protein_list': protein_list}
+    data = {'protein_list': protein_list,
+            'error_list': error_proteins}
 
     return render(request, "ocular_proteome_db/results.html", context=data)
 
