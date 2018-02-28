f35d11460eb8805921b4b24ec692fc0de4502f8d
danielmachlab <daniel@hadlim.com>
2018-02-28 09:20:50 -0600

    fixed a bug that crashed program if first protein entered caused an error

--- OcularPDB/views.py
+++ OcularPDB/views.py
@@ -7,17 +7,14 @@ def results(request):
 
     protein_array = identifier.split(' ')  # split elements in input into different elements at a space
     error_proteins = []  # list to store proteins entered but not found in database
+    protein_list = []  # add the RetinaProtein object to list which will hold all the proteins
 
-    print(protein_array[0])
-    protein = RetinaProtein.objects.get(pk=protein_array[0])  # return a RetinaProtein object
-    protein_list = [protein]  # add the RetinaProtein object to list which will hold all the proteins
-
-    for i in range(1, len(protein_array)):
+    for i in range(len(protein_array)):
         try:
             protein = RetinaProtein.objects.get(pk=protein_array[i])  # get next Protein object
             protein_list.append(protein)  # append new protein to list
         except:
-            print("error", protein_array[i])
+            print("error",  protein_array[i])
             error_proteins.append(protein_array[i] + ' ')  # if the protein is not found in the database, add it to the list
 
     data = {'protein_list': protein_list,
