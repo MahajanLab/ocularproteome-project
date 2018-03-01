cd4253e5b52d9b4287e93bcd5116698d38ffbaa1
danielmachlab <daniel@hadlim.com>
2018-03-01 10:44:27 -0600

    flag error only if protein not found in any dataset, display dataset column, do not duplicate error message for each dataset a protein is not found in.

--- OcularPDB/views.py
+++ OcularPDB/views.py
@@ -9,21 +9,32 @@ def results(request):
     protein_input = identifier.split(' ')  # split elements in input into different elements at a space
     error_proteins = []  # list to store proteins entered but not found in database
     protein_list = []  # add the RetinaProtein object to list which will hold all the proteins
+    protein_list_strings = []
 
     for i in range(len(protein_input)):
         p_found, p_error = RetinaProtein.search(protein_input[i])
+
         if p_found is None:
             error_proteins.append(p_error + ' ')  # if the protein is not found in the database, add it to the list
         else:
+            name = p_found.ens_id + ' '
+            # if name not in protein_list_strings:
             protein_list.append(p_found)  # append new protein to list
+            protein_list_strings.append(name)
 
         p_found, p_error = ChoroidProtein.search(protein_input[i])
+
         if p_found is None:
             error_proteins.append(p_error + ' ')  # if the protein is not found in the database, add it to the list
         else:
+            name = p_found.ens_id + ' '
+            # if name not in protein_list_strings:
             protein_list.append(p_found)  # append new protein to list
-            
+            protein_list_strings.append(name)
 
+    error_proteins = list(set(error_proteins))
+    print(protein_list_strings)
+    error_proteins = list(set(error_proteins)-set(protein_list_strings))
 
     data = {'protein_list': protein_list,
             'error_list': error_proteins}
