713dc9645fb29370326719a7fdd978fa564b7735
danielmachlab <daniel@hadlim.com>
2018-05-01 11:21:31 -0500

    done

--- ocular_proteome_db/settings.py
+++ ocular_proteome_db/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'OcularPDB/static/'
-MEDIA_ROOT
+
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
